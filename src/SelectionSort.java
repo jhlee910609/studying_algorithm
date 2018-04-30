dee26f5d42c3ccfd67f9d023a7374e37399891c5
JunHee Lee <jhlee910609@gmail.com>
2018-04-30 13:16:03 +0900

    add selection sort

---
+++ src/SelectionSort.java
@@ -0,0 +1,38 @@
+public class SelectionSort {
+
+    public static void main(String[] args) {
+        int[] a = {1, 34, 756, 8, 9, 12353, 7, 76, 57};
+        doSort(a);
+    }
+
+
+    public static void doSort(int[] a) {
+        int i, j, minIndex;
+
+        for (i = 0; i < a.length - 1; i++) {
+            System.out.println(i + "번쨰 === ");
+            minIndex = i;
+
+            for (j = i + 1; j < a.length; j++) {
+
+                if (a[j] < a[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            swap(a, minIndex, i);
+            System.out.println();
+//            for (int k = 0; k < a.length; k++) {
+//                System.out.print(a[k] + ", ");
+//            }
+        }
+    }
+
+    static void swap(int[] a, int min, int i) {
+        int temp = a[min];
+        a[min] = a[i];
+        a[i] = temp;
+        for (int k = 0; k < a.length; k++) {
+            System.out.print(a[k] + ", ");
+        }
+    }
+}
